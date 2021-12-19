fcb34941199e1abd5b35a8a98d57249705d23a8b
DancingSnow <1121149616@qq.com>
2021-12-19 15:40:22 +0800

    修改注册方法

--- src/main/java/cn/dancingsnow/create_sky/setup/Custom/item/Mechanisms.java
+++ src/main/java/cn/dancingsnow/create_sky/setup/Custom/item/Mechanisms.java
@@ -4,6 +4,7 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
@@ -11,17 +12,20 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class Mechanisms extends Item {
-    public Mechanisms(Properties properties) {
+    private final TextComponent text;
+    private final Boolean suggestion;
+    public Mechanisms(Properties properties, TranslationTextComponent textComponent, Boolean suggestion) {
         super(properties);
+        this.text = textComponent;
+        this.suggestion = suggestion;
     }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-
-
-
-        tooltip.add(new TranslationTextComponent(""));
-
+        tooltip.add(text);
+        if (suggestion == Boolean.TRUE) {
+            tooltip.add(new TranslationTextComponent("item.create_sky.mechanisms.suggestion"));
+        }
         super.appendHoverText(stack, world, tooltip, flagIn);
     }
 }
--- src/main/java/cn/dancingsnow/create_sky/setup/ModItems.java
+++ src/main/java/cn/dancingsnow/create_sky/setup/ModItems.java
@@ -1,7 +1,7 @@
 package cn.dancingsnow.create_sky.setup;
 
 import cn.dancingsnow.create_sky.CreateSky;
-import cn.dancingsnow.create_sky.setup.Custom.item.GetMechanisms;
+import cn.dancingsnow.create_sky.setup.Custom.item.Mechanisms;
 import net.minecraft.item.Item;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.RegistryObject;
@@ -10,37 +10,29 @@ public class ModItems {
 
     private static final String TOOLTIPS = "item." + CreateSky.MOD_ID + ".%s.tooltip";
 
-    public static final RegistryObject<Item> KINETIC_MECHANISM = Registration.ITEMS.register("kinetic_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "kinetic_mechanism")), Boolean.TRUE)
-    );
-    public static final RegistryObject<Item> SEALED_MECHANISM = Registration.ITEMS.register("sealed_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "sealed_mechanism")))
-    );
-    public static final RegistryObject<Item> INFERNAL_MECHANISM = Registration.ITEMS.register("infernal_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "infernal_mechanism")))
-    );
-    public static final RegistryObject<Item> INDUCTIVE_MECHANISM = Registration.ITEMS.register("inductive_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "inductive_mechanism")),Boolean.TRUE)
-    );
-    public static final RegistryObject<Item> ABSTRUSE_MECHANISM = Registration.ITEMS.register("abstruse_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "abstruse_mechanism")))
-    );
-    public static final RegistryObject<Item> CALCULATION_MECHANISM = Registration.ITEMS.register("calculation_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "calculation_mechanism")),Boolean.TRUE)
-    );
+    public static final RegistryObject<Item> KINETIC_MECHANISM = getMechanisms("kinetic_mechanism", Boolean.TRUE);
+    public static final RegistryObject<Item> SEALED_MECHANISM = getMechanisms("sealed_mechanism", Boolean.FALSE);
+    public static final RegistryObject<Item> INFERNAL_MECHANISM = getMechanisms("infernal_mechanism", Boolean.FALSE);
+    public static final RegistryObject<Item> INDUCTIVE_MECHANISM = getMechanisms("inductive_mechanism", Boolean.TRUE);
+    public static final RegistryObject<Item> ABSTRUSE_MECHANISM = getMechanisms("abstruse_mechanism", Boolean.FALSE);
+    public static final RegistryObject<Item> CALCULATION_MECHANISM = getMechanisms("calculation_mechanism", Boolean.TRUE);
 
-    public static final RegistryObject<Item> INCOMPLETE_KINETIC_MECHANISM = Registration.ITEMS.register("incomplete_kinetic_mechanism", () ->
-            GetMechanisms.getNewItemNoTab(new TranslationTextComponent(String.format(TOOLTIPS, "kinetic_mechanism")), Boolean.TRUE)
-    );
-    public static final RegistryObject<Item> INCOMPLETE_INFERNAL_MECHANISM = Registration.ITEMS.register("incomplete_infernal_mechanism", () ->
-            GetMechanisms.getNewItemNoTab(new TranslationTextComponent(String.format(TOOLTIPS, "incomplete_infernal_mechanism")))
-    );
-    public static final RegistryObject<Item> INCOMPLETE_INDUCTIVE_MECHANISM = Registration.ITEMS.register("incomplete_inductive_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "incomplete_inductive_mechanism")),Boolean.TRUE)
-    );
-    public static final RegistryObject<Item> INCOMPLETE_CALCULATION_MECHANISM = Registration.ITEMS.register("incomplete_calculation_mechanism", () ->
-            GetMechanisms.getNewItem(new TranslationTextComponent(String.format(TOOLTIPS, "incomplete_calculation_mechanism")),Boolean.TRUE)
-    );
+    public static final RegistryObject<Item> INCOMPLETE_KINETIC_MECHANISM = getMechanismsNotab("incomplete_kinetic_mechanism", Boolean.TRUE);
+    public static final RegistryObject<Item> INCOMPLETE_INFERNAL_MECHANISM = getMechanismsNotab("incomplete_infernal_mechanism", Boolean.FALSE);
+    public static final RegistryObject<Item> INCOMPLETE_INDUCTIVE_MECHANISM = getMechanismsNotab("incomplete_inductive_mechanism", Boolean.TRUE);
+    public static final RegistryObject<Item> INCOMPLETE_CALCULATION_MECHANISM = getMechanismsNotab("incomplete_calculation_mechanism", Boolean.TRUE);
 
     static void register() {}
+
+    public static RegistryObject<Item> getMechanisms(String reg_id, Boolean suggestion){
+        TranslationTextComponent textComponent = new TranslationTextComponent(String.format(TOOLTIPS, reg_id));
+        return Registration.ITEMS.register(reg_id, () ->
+                new Mechanisms(new Item.Properties().tab(ModGroups.ADDITIONS), textComponent, suggestion));
+    }
+
+    public static RegistryObject<Item> getMechanismsNotab(String reg_id, Boolean suggestion){
+        TranslationTextComponent textComponent = new TranslationTextComponent(String.format(TOOLTIPS, reg_id));
+        return Registration.ITEMS.register(reg_id, () ->
+                new Mechanisms(new Item.Properties(), textComponent, suggestion));
+    }
 }
