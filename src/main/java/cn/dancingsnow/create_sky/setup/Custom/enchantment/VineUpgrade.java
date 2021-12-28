79f3d60354609bb2918e1fd92bc056389ee8fd51
DancingSnow <1121149616@qq.com>
2021-12-28 14:00:34 +0800

    连锁附魔消耗

--- src/main/java/cn/dancingsnow/create_sky/setup/Custom/enchantment/VineUpgrade.java
+++ src/main/java/cn/dancingsnow/create_sky/setup/Custom/enchantment/VineUpgrade.java
@@ -20,5 +20,13 @@ public class VineUpgrade extends Enchantment {
         return 3;
     }
 
+    @Override
+    public int getMinCost(int p_77321_1_) {
+        return 15;
+    }
 
+    @Override
+    public int getMaxCost(int p_223551_1_) {
+        return getMinCost(p_223551_1_) + 50;
+    }
 }
