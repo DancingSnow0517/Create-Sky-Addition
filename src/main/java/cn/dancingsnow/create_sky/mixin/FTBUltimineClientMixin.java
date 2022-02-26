a0e2ca62aa039853371baad72835a307280194c2
DancingSnow <1121149616@qq.com>
2022-02-27 01:38:09 +0800

    完成FTBUltimine的Mixin

--- src/main/java/cn/dancingsnow/create_sky/mixin/FTBUltimineClientMixin.java
+++ src/main/java/cn/dancingsnow/create_sky/mixin/FTBUltimineClientMixin.java
@@ -1,6 +1,6 @@
 package cn.dancingsnow.create_sky.mixin;
 
-import cn.dancingsnow.create_sky.CreateSky;
+import cn.dancingsnow.create_sky.setup.ModEnchantments;
 import dev.ftb.mods.ftbultimine.client.FTBUltimineClient;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
@@ -8,27 +8,25 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
-import java.util.Objects;
 
-@Mixin(FTBUltimineClient.class)
+@Mixin(value = FTBUltimineClient.class, remap = false)
 public abstract class FTBUltimineClientMixin {
 
-    @Inject(at = @At("HEAD"), method = "clientTick")
-    private void clientTick(Minecraft mc, CallbackInfo ci) {
-        if (Minecraft.getInstance().player != null) {
-            ClientPlayerEntity player = Minecraft.getInstance().player;
+    @Shadow private boolean canUltimine;
+
+    @Inject(method = "clientTick", at = @At("RETURN"))
+    private void afterCanUltimine(Minecraft mc, CallbackInfo ci) {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        if (player != null) {
             ItemStack itemStack = player.getMainHandItem();
             Map<Enchantment, Integer> map = EnchantmentHelper.getEnchantments(itemStack);
-            map.forEach((enchantment, integer) -> {
-
-                CreateSky.LOGGER.info("Enchantment {}", Objects.requireNonNull(enchantment.getRegistryName()).toString());
-
-            });
+            canUltimine = map.containsKey(ModEnchantments.VEIN_UPGRADE.get());
         }
     }
 
